5d340047834d1310e847cc05648abf3acc0ea577
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-27 23:22:10 +0300

    Install gulp-webp-html-nosvg

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,10 @@
 import fileInclude from "gulp-file-include";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
         .pipe(fileInclude())
+        .pipe(webpHtmlNosvg())
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
         .pipe(app.gulp.dest(app.path.build.html))
 }
