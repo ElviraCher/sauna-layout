0c657a37400076b66c2bddd997a17ac54652c207
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-28 23:07:12 +0300

    Add gulp-if plugin

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -3,11 +3,13 @@ import plumber from "gulp-plumber"; // Обработка ошибок
 import notify from "gulp-notify";
 import browsersync from "browser-sync";
 import newer from "gulp-newer";
+import ifPlugin from "gulp-if"; //Условное ветвление
 
 export const plugins = {
    replace: replace,
    plumber: plumber,
    notify: notify,
    browsersync: browsersync,
-   newer: newer
+   newer: newer,
+   if: ifPlugin
 }
