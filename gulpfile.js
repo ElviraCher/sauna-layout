7dd1bc03453fa4c2de884aa1ba40106a6c4942b4
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-28 22:33:01 +0300

    Add svgSprite import and export

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprite } from "./gulp/tasks/svgSprite.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
@@ -24,6 +25,8 @@ function watcher() {
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch.images, images);
 }
+
+export { svgSprite } //чтобы можно было отдельно вызывать команду для svg
 
 //Последовательня обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
