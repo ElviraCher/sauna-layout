c8fb3c0f4b45a810333484b42425a850f0b65f94
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-28 19:08:13 +0300

    Import fonts tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
@@ -23,9 +24,15 @@ function watcher() {
     gulp.watch(path.watch.js, js);
     gulp.watch(path.watch.images, images);
 }
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+
+//Последовательня обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
+//Основные задачи
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 //Сценарии выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
+//Сценарий по умолчанию
 gulp.task("default", dev);
