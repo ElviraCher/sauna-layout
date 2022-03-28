829ccc32d4aa911f84ab06c5ab46dae12c9327c9
Elvira Cherkasova <ellisha25sheri@gmail.com>
2022-03-28 13:08:13 +0300

    Add plugins in scss function

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,6 +1,10 @@
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
 import rename from "gulp-rename";
+import cleanCss from "gulp-clean-css";
+import webpcss from "gulp-webpcss"; //вывод изображений в формате webp
+import autoPrefixer from "gulp-autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
 
 const sass = gulpSass(dartSass);
 
@@ -16,6 +20,20 @@ export const scss = () => {
    .pipe(sass({
       outputStyle: 'expanded'
    }))
+   .pipe(groupCssMediaQueries())
+   .pipe(webpcss(
+      {
+         webpClass: ".webp", //если браузер поддерживает формат webp, то добавляем класс
+         noWebpClass: ".no-webp"
+      }
+   ))
+   .pipe(autoPrefixer({
+      grid: true,
+      overrideBrowserlist: ["last 3 versions"],
+      cascade: true
+   }))
+   .pipe(app.gulp.dest(app.path.build.css)) //если нужен не сжатый файлик со стилями
+   .pipe(cleanCss())
    .pipe(rename({
       extname: ".min.css"
    }))
